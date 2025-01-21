512502d028049898b13beeef6ffe94c2f0d0187e
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-01-21 14:37:04 +0000

    WIP main cleanup

--- main.cpp
+++ main.cpp
@@ -5,11 +5,9 @@
 #include "include/components/init_comp.hpp"
 #include "include/threads.hpp"
 
-
 int main() {
-  CubeSystem c; // System struct
+  CubeSystem c;
 
-  // Scheduliing attributes
   pthread_attr_t initAttributes;
   pthread_attr_t displayAttr;
   pthread_attr_t RRthreads;
@@ -38,8 +36,15 @@ int main() {
   pthread_attr_setinheritsched(&displayAttr, PTHREAD_EXPLICIT_SCHED);
   pthread_attr_setinheritsched(&RRthreads, PTHREAD_EXPLICIT_SCHED);
 
-  // Initialize Cube system
   pthread_t initCubeSystemThread;
+  pthread_t displayCubeThread;
+  pthread_t readButtonsThread;
+  pthread_t updateSnakeDirectionsThreads;
+  pthread_t systemTransitionsThread;
+  pthread_t systemActionsThread;
+  pthread_t resetThread;
+
+  // Hardware initialization
   if (pthread_create(&initCubeSystemThread, &initAttributes, createCubeSystem,
                      (void *)&c) != 0) {
     printf("[ERROR] - Cannot create initCubeSystemThread\n");
@@ -47,52 +52,11 @@ int main() {
   }
   pthread_join(initCubeSystemThread, NULL);
 
-  // Update led display
-  pthread_t displayCubeThread;
-  if (pthread_create(&displayCubeThread, &displayAttr, displayCube, (void *)&c) !=
-      0) {
-    printf("[ERROR] - Cannot create displayCube\n");
-    exit(EXIT_FAILURE);
-  }
+  while (1) {
 
-  // read Buttons
-  pthread_t readButtonsThread;
-  if (pthread_create(&readButtonsThread, &RRthreads, readButtons, (void *)&c) !=
-      0) {
-    printf("[ERROR] - Cannot create readButtonsThread\n");
-    exit(EXIT_FAILURE);
+    pthread_create(&displayCubeThread, &displayAttr, displayCube, (void *)&c);
   }
-
-  pthread_t updateSnakeDirectionsThreads;
-  if (pthread_create(&updateSnakeDirectionsThreads, &RRthreads,
-                     updateSnakeDirection, (void *)&c) != 0) {
-    printf("[ERROR] - Cannot create updateSnakeDirectionsThreads\n");
-    exit(EXIT_FAILURE);
-  }
-
-  pthread_t systemTransitionsThread;
-  if (pthread_create(&systemTransitionsThread, &RRthreads,
-                     systemStateTransitions, (void *)&c) != 0) {
-    printf("[ERROR] - Cannot create readButtonsThread\n");
-    exit(EXIT_FAILURE);
-  }
-
-  pthread_t systemActionsThread;
-  if (pthread_create(&systemTransitionsThread, &RRthreads, systemStateActions,
-                     (void *)&c) != 0) {
-    printf("[ERROR] - Cannot create systemActionsThread\n");
-    exit(EXIT_FAILURE);
-  }
-
   pthread_join(displayCubeThread, NULL);
-
-  // reset expanders and shifters to exit program
-  pthread_t resetThread;
-  if (pthread_create(&resetThread, NULL, globalReset, (void *)&c) != 0) {
-    printf("[ERROR] - Cannot create resetThread\n");
-    exit(EXIT_FAILURE);
-  }
-  pthread_join(resetThread, NULL);
 
   pthread_attr_destroy(&initAttributes);
   pthread_attr_destroy(&RRthreads);
