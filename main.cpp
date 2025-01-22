1cfd765e62f9422a8e67033c73444ccfa98a3a7f
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-01-22 17:09:04 +0000

    updated main to use static cyclic scheduling

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include "include/components/init_comp.hpp"
 #include "include/threads.hpp"
 
+void sync(timespec startTime);
+
 int main() {
   CubeSystem c;
 
@@ -37,14 +39,13 @@ int main() {
   pthread_attr_setinheritsched(&RRthreads, PTHREAD_EXPLICIT_SCHED);
 
   pthread_t initCubeSystemThread;
-  pthread_t displayCubeThread;
-  pthread_t readButtonsThread;
-  pthread_t updateSnakeDirectionsThreads;
-  pthread_t systemTransitionsThread;
-  pthread_t systemActionsThread;
-  pthread_t resetThread;
-
+  pthread_t micro1thread;
+  pthread_t micro2thread;
+  pthread_t micro3thread;
   // Hardware initialization
+  // Runs once
+  // WARN: one-shot thread!
+
   if (pthread_create(&initCubeSystemThread, &initAttributes, createCubeSystem,
                      (void *)&c) != 0) {
     printf("[ERROR] - Cannot create initCubeSystemThread\n");
@@ -52,12 +53,77 @@ int main() {
   }
   pthread_join(initCubeSystemThread, NULL);
 
+  // sync
+  struct timespec startTime, currentTime;
+  // initialize Timer
+  long ellapsedTimeMs = 0;
+  long iterationTimeMs = 0;
+
+  clock_gettime(CLOCK_MONOTONIC, &startTime);
+
+  printf("Entering loop.\n");
+
   while (1) {
+    printf("Starting micro1thread\n");
+    clock_gettime(CLOCK_MONOTONIC, &currentTime);
+    pthread_create(&micro1thread, &displayAttr, micro1, (void *)&c);
+    pthread_join(micro1thread, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &currentTime);
 
-    pthread_create(&displayCubeThread, &displayAttr, displayCube, (void *)&c);
+    ellapsedTimeMs = (currentTime.tv_sec - startTime.tv_sec) * 1000;
+    ellapsedTimeMs += (currentTime.tv_nsec - startTime.tv_nsec) / 1'000'000;
+    printf("ellapsedTimeMs: %ld ms.\n", ellapsedTimeMs);
+
+    if (ellapsedTimeMs < 100) {
+      usleep(100 - ellapsedTimeMs);
+      printf("ellapsedTime is < 100, sleeping for %ld ms.\n",
+             100 - ellapsedTimeMs);
+      clock_gettime(CLOCK_MONOTONIC, &startTime);
+      /* continue; */
+    }
+
+    printf("Starting micro2thread\n");
+    clock_gettime(CLOCK_MONOTONIC, &currentTime);
+    pthread_create(&micro2thread, &displayAttr, micro2, (void *)&c);
+    pthread_join(micro2thread, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &currentTime);
+
+    ellapsedTimeMs = (currentTime.tv_sec - startTime.tv_sec) * 1000;
+    ellapsedTimeMs += (currentTime.tv_nsec - startTime.tv_nsec) / 1'000'000;
+    printf("ellapsedTimeMs: %ld ms.\n", ellapsedTimeMs);
+
+    if (ellapsedTimeMs < 100) {
+      usleep(100 - ellapsedTimeMs);
+      printf("ellapsedTime is < 100, sleeping for %ld ms.\n",
+             100 - ellapsedTimeMs);
+      clock_gettime(CLOCK_MONOTONIC, &startTime);
+      /* continue; */
+    }
+
+    printf("Starting micro3thread\n");
+    clock_gettime(CLOCK_MONOTONIC, &currentTime);
+    pthread_create(&micro3thread, &displayAttr, micro3, (void *)&c);
+    pthread_join(micro3thread, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &currentTime);
+
+    ellapsedTimeMs = (currentTime.tv_sec - startTime.tv_sec) * 1000;
+    ellapsedTimeMs += (currentTime.tv_nsec - startTime.tv_nsec) / 1'000'000;
+    printf("ellapsedTimeMs: %ld ms.\n", ellapsedTimeMs);
+
+    if (ellapsedTimeMs < 100) {
+      usleep(100 - ellapsedTimeMs);
+      printf("ellapsedTime is < 100, sleeping for %ld ms.\n",
+             100 - ellapsedTimeMs);
+      clock_gettime(CLOCK_MONOTONIC, &startTime);
+      continue;
+    }
+    if (ellapsedTimeMs >= 100) {
+      printf("Breaking loop, ellapsedTimeMs is %ld ms.\n", ellapsedTimeMs);
+      break;
+    }
   }
-  pthread_join(displayCubeThread, NULL);
 
+  // Cleanup before ending process
   pthread_attr_destroy(&initAttributes);
   pthread_attr_destroy(&RRthreads);
 
